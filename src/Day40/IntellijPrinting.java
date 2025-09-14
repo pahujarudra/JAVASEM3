95ef098162c67c5e6c9a0779c810fee542db6612
pahujarudra <pahujarudra@gmail.com>
2025-09-14 11:25:56 +0530

    Day40

---
+++ src/Day40/IntellijPrinting.java
@@ -0,0 +1,15 @@
+package Day40;
+
+public class IntellijPrinting {
+    public static void main(String[] args) {
+        String str = "INTELLIJ";
+        // Outer loop - controls the starting character
+        for (int i = 0; i < str.length(); i++) {
+            // Inner loop - prints characters from current index to end
+            for (int j = i; j < str.length(); j++) {
+                System.out.print(str.charAt(j));
+            }
+            System.out.println(); // Move to next line
+        }
+    }
+}
