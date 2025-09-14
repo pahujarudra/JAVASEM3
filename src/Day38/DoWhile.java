0045bbe046783810a241c329ba7279280a859d79
pahujarudra <pahujarudra@gmail.com>
2025-09-14 11:20:46 +0530

    Day38

---
+++ src/Day38/DoWhile.java
@@ -0,0 +1,13 @@
+package Day38;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n=10;
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=n);
+    }
+}
---
+++ src/Day38/SwitchCase.java
@@ -0,0 +1,24 @@
+package Day38;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+
+        // Using if-else
+        if (marks >= 90) {
+            System.out.println("Excellent");
+        } else if (marks >= 75) {
+            System.out.println("Very Good");
+        } else if (marks >= 50) {
+            System.out.println("Good");
+        } else {
+            System.out.println("Fail");
+        }
+
+    }
+}
